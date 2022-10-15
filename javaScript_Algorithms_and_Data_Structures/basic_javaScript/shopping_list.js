5b4f7f7e8ef292b3a454f06b347ec0811864d68f
light crypt <40130186+yeonoel@users.noreply.github.com>
2022-10-15 01:39:48 +0000

    Add files via upload
    basic_javaScript

---
+++ javaScript_Algorithms_and_Data_Structures/basic_javaScript/shopping_list.js
@@ -0,0 +1,10 @@
+const myList = [
+    ["Chocolate Bar", 15],
+    ["Rice", 45],
+    ["coffee", 34],
+    ["Banana", 65],
+    ["bread", 20],
+    ["chilli pepper", 10],
+    ["cookies", 10]
+    
+    ];
