682f73c76930c7239b6c95a3ed237ac30d3256d3
yeonoel <yeopevroguinoel@gmail.com>
2022-10-18 18:40:49 +0000

    Record collection

---
+++ javaScript_Algorithms_and_Data_Structures/basic_javaScript/manipulating_complex_objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Burna Boy",
+    "title": "Plenty plenty",
+    "release_year": 2022,
+    "formats": [
+      "MP4",
+      "MP3",
+      "LP"
+    ],
+    "gold": true
+  }
+];
