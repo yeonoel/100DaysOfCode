d4047a7d05c2f4379a55972669c95f0f539def58
yeonoel <yeopevroguinoel@gmail.com>
2022-10-27 11:06:03 +0000

    Use getters and setters to Control Access to an Object

---
+++ javaScript_Algorithms_and_Data_Structures/ES6/use_getters_and_setters_object.js
@@ -0,0 +1,20 @@
+// Only change code below this line
+class Thermostat {
+  constructor(forei) {
+    this.forei = forei;
+
+}
+get temperature() {
+  return (5/9 * (this.forei - 32));
+}
+set temperature(temp) {
+  this.forei = temp * 9.0 / 5 + 32;
+  
+}
+}
+// Only change code above this line
+
+const thermos = new Thermostat(76); // Setting in Fahrenheit scale
+let temp = thermos.temperature; // 24.44 in Celsius
+thermos.temperature = 26;
+temp = thermos.temperature; // 26 in Celsius
